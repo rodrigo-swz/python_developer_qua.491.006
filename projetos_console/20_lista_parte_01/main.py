8285effbcedb5831060a71ead3f7b7e1d62c6c2d
rodrigo-swz <rodrigolopes.92@hotmail.com>
2025-06-18 21:29:28 -0300

    Commit do dia 18/06

---
+++ projetos_console/20_lista_parte_01/main.py
@@ -0,0 +1,14 @@
+nomes = ['Fulano', 'Ciclano', 'Beltrano', 'Jow', 'Juninho Pernambucano', "Klaus"]
+
+print(nomes[0])
+
+'''# imprime o número de itens
+print(len(nomes))'''
+
+
+#usando o for
+for nome in nomes:
+    print(nome)
+
+'''ordenamento alfabético:
+nomes.sort()'''
